ec3241c2fa647fcb23c84a4d6cb98ccee0858a96
LuisAngelGonzalez <luis.gonzalez@kleverness.com>
2022-10-22 19:55:59 -0500

    Add Patrones

--- src/App.java
+++ src/App.java
@@ -1,13 +1,15 @@
+import java.io.InputStream;
+import java.util.stream.IntStream;
+
+import Builder.CocheBuilder;
+import Builder.Vehiculo;
+import Factory.PrecioFactory;
+import Factory.PrecioUsd;
 import Singleton.Singleton;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        Singleton singleton = Singleton.getInstance();
-        Singleton singleton2 = Singleton.getInstance();
-        singleton.setCounter();
-        singleton.setCounter();
-        singleton2.setCounter();
-        System.err.println(singleton2.getCounter());
+
     }
+
 }
